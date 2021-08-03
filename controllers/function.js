d37f3b59d7fee71597e794969c0f6e07d0d58123
naranjito72 <theclick36@gmail.com>
2021-08-03 14:36:40 +0200

    time was up so leave it as it was

--- controllers/function.js
+++ controllers/function.js
@@ -27,8 +27,8 @@ const MAIN_FUNCTIONSTWO = {
     // dates
     getDatesTwo() {
         // clean arr
-        this.arrDatesTwo = [];
-        this.numbersFrontTwo = [];
+        MAIN_FUNCTIONSTWO.arrDatesTwo = [];
+        MAIN_FUNCTIONSTWO.numbersFrontTwo = [];
         // create new date
         let d = new Date();
         // empty fields | null
@@ -38,29 +38,29 @@ const MAIN_FUNCTIONSTWO = {
         AUX_FUNCTIONSTWO.pushToArr(MAIN_FUNCTIONSTWO.checkInDateTwo.value);
         AUX_FUNCTIONSTWO.pushToArr(MAIN_FUNCTIONSTWO.checkOutDateTwo.value);
         // check how many numbers infront
-        this.numbersFrontTwo = AUX_FUNCTIONSTWO.checkLengthFront(this.arrDatesTwo);
+        MAIN_FUNCTIONSTWO.numbersFrontTwo = AUX_FUNCTIONSTWO.checkLengthFront(MAIN_FUNCTIONSTWO.arrDatesTwo);
         // check regex
-        this.myBoolean = AUX_FUNCTIONSTWO.checkSymbol(MAIN_FUNCTIONSTWO.checkInDateTwo.value);
+        MAIN_FUNCTIONSTWO.myBoolean = AUX_FUNCTIONSTWO.checkSymbol(MAIN_FUNCTIONSTWO.checkInDateTwo.value);
         // check if date comes in dd-MM-yyyy
         // check both index as the arrays will be always the same length
         // how to refac with map?
         let i = 0;
-        for (i; i < this.arrDatesTwo.length; i++) {
-            if (this.numbersFrontTwo[i] == 2)
-                this.arrDatesTwo[i] = AUX_FUNCTIONSTWO.cleanAndReverseDate(this.arrDatesTwo[i]);
-            if (AUX_FUNCTIONSTWO.checkSymbol(this.arrDatesTwo[i]) == true)
-                this.arrDatesTwo[i] = AUX_FUNCTIONSTWO.cleanDate(this.arrDatesTwo[i]);
+        for (i; i < MAIN_FUNCTIONSTWO.arrDatesTwo.length; i++) {
+            if (MAIN_FUNCTIONSTWO.numbersFrontTwo[i] == 2)
+                MAIN_FUNCTIONSTWO.arrDatesTwo[i] = AUX_FUNCTIONSTWO.cleanAndReverseDate(MAIN_FUNCTIONSTWO.arrDatesTwo[i]);
+            if (AUX_FUNCTIONSTWO.checkSymbol(MAIN_FUNCTIONSTWO.arrDatesTwo[i]) == true)
+                MAIN_FUNCTIONSTWO.arrDatesTwo[i] = AUX_FUNCTIONSTWO.cleanDate(MAIN_FUNCTIONSTWO.arrDatesTwo[i]);
         }
         // what kind of guest are comming
         MAIN_FUNCTIONSTWO.kindGuestsTwo();
         // create the object
         MAIN_FUNCTIONSTWO.createDataTwo();
-        return this.arrDatesTwo;
+        return MAIN_FUNCTIONSTWO.arrDatesTwo;
     },
     // kind of guest
     kindGuestsTwo() {
-        this.myAdultsTwo = this.getPeopleSelectTwo(this.guestsAdultsTwo);
-        this.myKidsTwo = this.getPeopleSelectTwo(this.guestsKidsTwo);
+        MAIN_FUNCTIONSTWO.myAdultsTwo = MAIN_FUNCTIONSTWO.getPeopleSelectTwo(MAIN_FUNCTIONSTWO.guestsAdultsTwo);
+        MAIN_FUNCTIONSTWO.myKidsTwo = MAIN_FUNCTIONSTWO.getPeopleSelectTwo(MAIN_FUNCTIONSTWO.guestsKidsTwo);
     },
     // take values from html
     getPeopleSelectTwo(peopleTwo) {
@@ -72,19 +72,19 @@ const MAIN_FUNCTIONSTWO = {
     },
     // how many rooms checked
     numberOfRoomsChecked() {
-        this.seenRoomsTwo++;
-        this.newOneTwo.numberRoomsChecked(this.seenRoomsTwo);
-        console.log(this.newOneTwo.toString());
-        return this.seenRoomsTwo;
+        MAIN_FUNCTIONSTWO.seenRoomsTwo++;
+        MAIN_FUNCTIONSTWO.newOneTwo.numberRoomsChecked(MAIN_FUNCTIONSTWO.seenRoomsTwo);
+        console.log(MAIN_FUNCTIONSTWO.newOneTwo.toString());
+        return MAIN_FUNCTIONSTWO.seenRoomsTwo;
     },
     // show me the money
-    showPriceTwo: () => this.priceRoomTwo.map((e) => e = e.innerText).sort((a, b) => a - b),
+    showPriceTwo: () => MAIN_FUNCTIONSTWO.priceRoomTwo.map((e) => e = e.innerText).sort((a, b) => a - b),
     // create Object
     createDataTwo() {
-        this.newOneTwo = new Hotel(this.arrDatesTwo[0], this.arrDatesTwo[1], this.showPriceTwo[0], this.currencyHotelTwo, this.seenRoomsTwo, this.myAdultsTwo, this.myKidsTwo, this.getLanguageTwo());
+        MAIN_FUNCTIONSTWO.newOneTwo = new Hotel(MAIN_FUNCTIONSTWO.arrDatesTwo[0], MAIN_FUNCTIONSTWO.arrDatesTwo[1], MAIN_FUNCTIONSTWO.showPriceTwo[0], MAIN_FUNCTIONSTWO.currencyHotelTwo, MAIN_FUNCTIONSTWO.seenRoomsTwo, MAIN_FUNCTIONSTWO.myAdultsTwo, MAIN_FUNCTIONSTWO.myKidsTwo, MAIN_FUNCTIONSTWO.getLanguageTwo());
         // add all guests
-        this.totalGuestsTwo = this.newOneTwo.howManyGuests();
-        console.log(this.newOneTwo.toString());
+        MAIN_FUNCTIONSTWO.totalGuestsTwo = MAIN_FUNCTIONSTWO.newOneTwo.howManyGuests();
+        console.log(MAIN_FUNCTIONSTWO.newOneTwo.toString());
     }
 };
 const AUX_FUNCTIONSTWO = {
